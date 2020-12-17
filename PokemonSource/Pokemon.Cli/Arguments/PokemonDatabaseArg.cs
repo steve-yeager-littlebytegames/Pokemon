ca73c5baf6c3e5086160d45308199089c766775e
Steve Yeager <steve.yeager@littlebytegames.com>
2020-12-16 23:55:55 -0800

    Add pokemon cli arg

---
+++ PokemonSource/Pokemon.Cli/Arguments/PokemonDatabaseArg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CommandLine;
+using Pokemon.Core.Sim;
+
+namespace Pokemon.Cli.Arguments
+{
+    [Verb("pokemon", HelpText = "Show Pokemon database.")]
+    public class PokemonDatabaseArg
+    {
+        public int Run(CoreSim coreSim)
+        {
+            Console.WriteLine(coreSim.PokemonDatabase.ToString());
+            return 0;
+        }
+    }
+}
--- PokemonSource/Pokemon.Cli/Program.cs
+++ PokemonSource/Pokemon.Cli/Program.cs
@@ -1,21 +1,28 @@
-﻿using System;
+﻿using CommandLine;
+using Pokemon.Cli.Arguments;
 using Pokemon.Core.Sim;
 
 namespace Pokemon.Cli
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             var coreSim = new CoreSim("D:\\Projects\\Git\\Pokemon\\PokemonSource\\GameData");
+            coreSim.Load();
 
-            while(true)
-            {
-                coreSim.Load();
-                coreSim.Save();
-                Console.Write(coreSim.PokemonDatabase.ToString());
-                Console.ReadKey();
-            }
+            return Parser.Default.ParseArguments<PokemonDatabaseArg>(args)
+                .MapResult(arg => arg.Run(coreSim), errors => 1);
+
+            //var coreSim = new CoreSim("D:\\Projects\\Git\\Pokemon\\PokemonSource\\GameData");
+
+            //while(true)
+            //{
+            //    coreSim.Load();
+            //    coreSim.Save();
+            //    Console.Write(coreSim.PokemonDatabase.ToString());
+            //    Console.ReadKey();
+            //}
         }
     }
 }
